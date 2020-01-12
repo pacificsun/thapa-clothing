60d774b1efe7de6d0524e70745308d92b71af682
Suraj Thapa <stsurajst@gmail.com>
2020-01-12 23:09:45 +0545

    custom button added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './page/homepage/homepage.component';
 import ShopPage from './page/shop/shop.component';
+import SignInAndSignUp from './page/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 const HatsPage = () => (
   <div>
@@ -17,6 +18,7 @@ function App() {
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop/hats" component={HatsPage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInAndSignUp} />
       </Switch>
     </div>
   );
