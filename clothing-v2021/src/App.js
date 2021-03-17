f71645ce87e737fb8eaee33cebaacd3925162fe6
Suraj Thapa <stsurajst@gmail.com>
2021-03-17 22:33:05 +0545

    url path fix withRouter and route section complete

--- clothing-v2021/src/App.js
+++ clothing-v2021/src/App.js
@@ -1,10 +1,21 @@
+import { Route, Switch } from "react-router-dom";
+
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 
+const HatPage = () => (
+  <div>
+    <h1>HAT PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatPage} />
+      </Switch>
     </div>
   );
 }
