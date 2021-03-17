2800192d8e8a215b5f102b295f6add2c406ba210
Suraj Thapa <stsurajst@gmail.com>
2021-03-17 23:57:26 +0545

    updated our app with a shop page, collection preview and collection item components

--- clothing-v2021/src/App.js
+++ clothing-v2021/src/App.js
@@ -2,19 +2,14 @@ import { Route, Switch } from "react-router-dom";
 
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
-
-const HatPage = () => (
-  <div>
-    <h1>HAT PAGE</h1>
-  </div>
-);
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ clothing-v2021/src/pages/shop/shop.component.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+import SHOP_DATA from "./shop.data";
+
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+
+const ShopPage = () => {
+  const [collection, setCollection] = useState(SHOP_DATA);
+
+  return (
+    <div className="shop-page">
+      {collection.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  );
+};
+
+export default ShopPage;
